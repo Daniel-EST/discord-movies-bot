5de9cc36e7acd6e4f8672c13f90dc92a5e547d9f
Daniel-EST <ddsantos@cos.ufrj.br>
2023-05-06 18:36:29 -0300

    feat: adds remover_filme command

--- src/commands/_commandList.ts
+++ src/commands/_commandList.ts
@@ -1,4 +1,4 @@
 import { type Command } from '../interfaces/commands'
-import { listMovies, infoMovie, addMovie, randomMovie } from './movies'
+import { listMovies, infoMovie, addMovie, randomMovie, removeMovie } from './movies'
 
-export const CommandList: Command[] = [infoMovie, listMovies, addMovie, randomMovie]
+export const CommandList: Command[] = [infoMovie, listMovies, addMovie, randomMovie, removeMovie]
